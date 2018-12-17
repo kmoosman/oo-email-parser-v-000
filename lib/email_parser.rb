4f22625bbf9b174863e46a83bf2df038b4d1c9d8
Katie Moosman <moosman.katie@gmail.com>
2018-12-17 03:01:01 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,6 +3,10 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailParser(emails)
+class EmailParser
+  
+  def self.parse 
+    
+  end
   
 end
