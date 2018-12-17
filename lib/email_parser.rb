480db7dd137dcdfba35a6bef5966b2bb2a287963
Katie Moosman <moosman.katie@gmail.com>
2018-12-17 03:32:39 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,12 +9,13 @@ class EmailParser
   
   def self.parse(emails) 
     separated_emails = emails.split(" ", ",")
-    artist_name = data[0]
-    song_name = data[1].gsub(".mp3", "")
-    song = self.new
-    song.name = song_name
-    song.artist_name = artist_name
-    song
+    binding.pry
+    # artist_name = data[0]
+    # song_name = data[1].gsub(".mp3", "")
+    # song = self.new
+    # song.name = song_name
+    # song.artist_name = artist_name
+    # song
   end
   
 end
